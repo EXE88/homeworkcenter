f77873895e0e348e6d8b04d4d3874090d0cf9415
danial <bobolk1388@gmail.com>
2023-11-08 17:42:46 +0330

    transfer forms from main app to homework app

--- homework/views.py
+++ homework/views.py
@@ -3,5 +3,7 @@ from rest_framework.views import APIView
 from . import forms
 
 class CreateNewHomework(APIView):
+    form_class = forms.WriteHomeworkForm
     def get(self,request):
-        return render(request,'homework/createnewhomework.html')
+        form = self.form_class()
+        return render(request,'homework/createnewhomework.html',{"form":form,"day":form.tomorrow.strftime('%A')})
