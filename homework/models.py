f822d5ace84b2f9eb36498323c6e58ab799f0f24
danial <bobolk1388@gmail.com>
2023-11-11 17:22:15 +0330

    complete write homework page with style and systems

--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class WriteHomework(models.Model):
+    date = models.DateField(blank=False)
+    math = models.CharField(max_length=100 , blank=True)
+    literature = models.CharField(max_length=100 , blank=True)
+    biology = models.CharField(max_length=100 , blank=True)
+    physics = models.CharField(max_length=100 , blank=True)
+    religious = models.CharField(max_length=100 , blank=True)
+    Defense_readiness = models.CharField(max_length=100 , blank=True)
+    Social_studies = models.CharField(max_length=100 , blank=True)
+    english = models.CharField(max_length=100 , blank=True)
+    conversation = models.CharField(max_length=100 , blank=True)
+    arabic = models.CharField(max_length=100 , blank=True)
+    quran = models.CharField(max_length=100 , blank=True)
+    writeing = models.CharField(max_length=100 , blank=True)
+    art = models.CharField(max_length=100 , blank=True)
--- homework/views.py
+++ homework/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from rest_framework.views import APIView
 from . import forms
+from . import models
+from jdatetime import date
 
 class CreateNewHomework(APIView):
     form_class = forms.WriteHomeworkForm
@@ -11,5 +13,22 @@ class CreateNewHomework(APIView):
     def post(self,request):
         form = self.form_class(request.POST)
         if form.is_valid():
-            messages.success(request,'successful')
+            cd = form.cleaned_data
+            new_homework = models.WriteHomework.objects.create(
+            date = cd['date'],
+            math = cd['math'],
+            literature = cd['literature'],
+            biology = cd['biology'],
+            physics = cd['physics'],
+            religious = cd['religious'],
+            Defense_readiness = cd['Defense_readiness'],
+            Social_studies = cd['Social_studies'],
+            english = cd['english'],
+            conversation = cd['conversation'],
+            arabic = cd['arabic'],
+            quran = cd['quran'],
+            writeing = cd['writeing'],
+            art = cd['art'],
+            )
+            messages.success(request,'successful','success')
             return redirect('main_page')
