cc78b87d98a1fcea790952b516356a32814c3a2e
danial <bobolk1388@gmail.com>
2023-11-13 16:56:13 +0330

    complete main page and admin page and add select date

--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render , redirect
 from rest_framework.views import APIView
+from homework import models
+from jdatetime import date
+from datetime import timedelta
+from googletrans import Translator
 
 class MainPage(APIView):
     def get(self,request):
-        return render(request,'main/mainpage.html')
+        allmodels = models.WriteHomework.objects.all()
+        for model in allmodels:
+            miladi_date = date(year=model.date.year,month=model.date.month,day=model.date.day).togregorian()
+            shamsi_date = date.fromgregorian(year = miladi_date.year , month = miladi_date.month , day = miladi_date.day)
+            model.date = f"{shamsi_date.year}/{shamsi_date.month}/{shamsi_date.day} {shamsi_date.strftime('%A')}"
+            translator = Translator()
+            model.date = translator.translate(text=model.date,dest='fa',src='en').text 
+        sorted_models = sorted(allmodels, key=lambda x: x.date, reverse=True)
+        return render(request,'main/mainpage.html',{"models":sorted_models})
