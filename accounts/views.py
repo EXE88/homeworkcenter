5d13fa2979f0707156b2ebc4a36039912020dbfd
danial <bobolk1388@gmail.com>
2023-11-14 16:06:05 +0330

    create blank view classes for accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
 
-# Create your views here.
+class UserRegisterView(APIView):
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
+    
+class UserLoginrView(APIView):
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
+    
+class UserLogoutView(APIView):
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
