4f656974266b550e58da487728782c3723038c87
danial <bobolk1388@gmail.com>
2023-11-14 16:01:05 +0330

    create url for register , login and logout pages

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/',views.UserRegisterView.as_view(),name='register_page'),
+    path('login/',views.UserLoginView.as_view(),name='login_page'),
+    path('logout/',views.UserLogoutView.as_view(),name='logout_page'),
+]
