51eddaff393063a58907ed82593d4fbbbfa098af
danial <bobolk1388@gmail.com>
2023-11-15 16:15:09 +0330

    complete user registeration system

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField(max_length=50 , required=True , widget=forms.TextInput(attrs={}))
-    email = forms.EmailField(required=True , widget=forms.EmailInput(attrs={}))
-    password = forms.CharField(required=True , widget=forms.PasswordInput(attrs={}))
-    password_verify = forms.CharField(required=True , widget=forms.PasswordInput(attrs={}))
+    username = forms.CharField(max_length=25 , required=True , widget=forms.TextInput(attrs={"class":"form-control"}))
+    email = forms.EmailField(required=True , widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password = forms.CharField(required=True , widget=forms.PasswordInput(attrs={"class":"form-control"}) , max_length=15 , min_length=8)
+    password_verify = forms.CharField(required=True , widget=forms.PasswordInput(attrs={"class":"form-control"}) , max_length=15 , min_length=8)
     
-    def validate_email(self):
+    def clean_email(self):
         email = self.cleaned_data['email']
-        user = User.objects.filter(email=email).exists()
-        if user:
+        email_exists = User.objects.filter(email=email).exists()
+        if email_exists:
             raise ValidationError('this email already exists')
         return email
     
@@ -19,7 +19,11 @@ class UserRegisterForm(forms.Form):
         cd =  super().clean()
         password = cd.get('password')
         password_verify = cd.get('password_verify')
+        username = cd.get('username')
         
+        username_exists = User.objects.filter(username=username).exists()
         if password and password_verify and password != password_verify:
             raise ValidationError('the passwords must be match')
+        if username_exists:
+            raise ValidationError('there is a user with this username')
         
--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from . import forms
+from django.contrib import messages
+from django.shortcuts import redirect , render
 
 class UserRegisterView(APIView):
     form_class = forms.UserRegisterForm
+    template_name = "accounts/registerpage.html"
     def get(self,request):
         form = self.form_class()
         return render(request,'accounts/registerpage.html',{'form':form})
     def post(self,request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'],email=cd['email'],password=cd['password'])
+            messages.success(request,'you are registered successfully','success')
+            return redirect('main_page')
+        return render(request,self.template_name,{"form":form})
     
 class UserLoginView(APIView):
     def get(self,request):
