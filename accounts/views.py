5f8fd2f1fb5009724c991015ac05b074f915914e
danial <bobolk1388@gmail.com>
2023-11-15 15:02:17 +0330

    send form to template

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
+from . import forms
 
 class UserRegisterView(APIView):
+    form_class = forms.UserRegisterForm
     def get(self,request):
-        return render(request,'accounts/registerpage.html')
+        form = self.form_class()
+        return render(request,'accounts/registerpage.html',{'form':form})
     def post(self,request):
         pass
     
