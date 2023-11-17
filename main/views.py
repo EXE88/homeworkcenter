571591cde01939d725c51b2bab317b5f009b2708
danial <bobolk1388@gmail.com>
2023-11-17 23:04:23 +0330

    change main pages style name and add vazir font and make day translation faster

--- main/views.py
+++ main/views.py
@@ -13,7 +13,22 @@ class MainPage(LoginRequiredMixin,View):
             miladi_date = date(year=model.date.year,month=model.date.month,day=model.date.day).togregorian()
             shamsi_date = date.fromgregorian(year = miladi_date.year , month = miladi_date.month , day = miladi_date.day)
             model.date = f"{shamsi_date.year}/{shamsi_date.month}/{shamsi_date.day} {shamsi_date.strftime('%A')}"
-            translator = Translator()
-            model.date = translator.translate(text=model.date,dest='fa',src='en').text 
+            
+            full_date = model.date
+            if "Saturday" in full_date:
+                model.date = full_date.replace('Saturday','شنبه')
+            if "Sunday" in full_date:
+                model.date = full_date.replace('Sunday','یکشنبه')
+            if "Monday" in full_date:
+                model.date = full_date.replace('Monday','دوشنبه')
+            if "Tuesday" in full_date:
+                model.date = full_date.replace('Tuesday','سه شنبه')
+            if "Wednesday" in full_date:
+                model.date = full_date.replace('Wednesday','چهارشنبه')
+            if "Thursday" in full_date:
+                model.date = full_date.replace('Thursday','پنجشنبه (تعطیل)')
+            if "Friday" in full_date:
+                model.date = full_date.replace('Friday','جمعه (تعطیل)')
+        
         sorted_models = sorted(allmodels, key=lambda x: x.date, reverse=True)
         return render(request,'main/mainpage.html',{"models":sorted_models})
